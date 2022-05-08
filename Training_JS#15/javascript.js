cb50fd0f55e434bbc669ad57f8bf4094881e53d8
Stanislau Kakhnovich <stas_2002@tut.by>
2022-05-08 13:58:55 +0300

    correct solution

--- Training_JS#15/javascript.js
+++ Training_JS#15/javascript.js
@@ -13,5 +13,12 @@ howManySmaller([1.1888,1.1868,1.1838],1.19) should return 1
 howManySmaller([3.1288,3.1212,3.1205],3.1212) should return 2
 */
 
+function howManySmaller(arr,n){
+ return arr.map(elem=>elem.toFixed(2)).filter(elem=>elem<n).length
+  
+}
 
-  console.log();
+
+console.log(howManySmaller([1.234,1.235,1.228],1.24));
+console.log(howManySmaller([1.1888,1.1868,1.1838],1.19));
+console.log(howManySmaller([3.1288,3.1212,3.1205],3.1212));
