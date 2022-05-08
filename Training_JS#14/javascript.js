bcb0f0ad8b801d70df020050df333c09603b4cfa
Stanislau Kakhnovich <stas_2002@tut.by>
2022-05-08 13:29:20 +0300

    correct solution

--- Training_JS#14/javascript.js
+++ Training_JS#14/javascript.js
@@ -1,4 +1,11 @@
+//rgb to hex
 
+function colorOf(r,g,b){
+  let red = r.toString(16);
+  let green = g.toString(16);
+  let blue = b.toString(16);
+  let hex = `#${red.length == 1?0+red:red}${green.length == 1?0+green:green}${blue.length == 1?0+blue:blue}`
+  return hex;
+}
 
-
-  console.log(whatNumberIsIt(5/0));
+  console.log(colorOf(255,10,0));
