2d4573c7cbad1c6fd3f487c43a825d93062f1dae
Stanislau Kakhnovich <stas_2002@tut.by>
2022-06-17 12:28:40 +0300

    correct solution

--- DoubleInteger/javascript.js
+++ DoubleInteger/javascript.js
@@ -1,9 +1,9 @@
 // Code as fast as you can! You need to double the integer and return it.
 
 function doubleInteger(i) {
-  // i will be an integer. Double it and return it.
+  i*=2;
   return i;
 }
 
-console.log(doubleInteger(i));
+console.log(doubleInteger(5));
 
