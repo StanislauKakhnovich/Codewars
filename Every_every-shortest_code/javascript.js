d4bb40700823adddb030086e0f351838be5d095b
Stanislau Kakhnovich <stas_2002@tut.by>
2022-06-22 12:36:59 +0300

    the task

---
+++ Every_every-shortest_code/javascript.js
@@ -0,0 +1,14 @@
+// You are provided a 2 dimensional array. You must test if each value within the sub arrays is larger than the given test value n.
+
+// Return true if so, false if not.
+
+// Code length limit : *** 60 characters ***
+
+function everyEvery(a,n){
+
+}
+
+
+console.log(everyEvery([[66, 101], [99, 88], [77, 10]], 100));
+console.log(everyEvery([[78, 117], [110, 99, 104], [117, 107, 115]], 8));
+
