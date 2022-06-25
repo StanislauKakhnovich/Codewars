1cb9395febb1d07a56ff3da1f55bc55bb95e0950
Stanislau Kakhnovich <stas_2002@tut.by>
2022-06-25 10:43:52 +0300

    correct solution

--- Opposites_Attract/javascript.js
+++ Opposites_Attract/javascript.js
@@ -5,7 +5,9 @@
 // false if they aren't.
 
 function lovefunc(flower1, flower2){
-  // moment of truth
+  if (flower1%2==0 && flower2%2!=0) return true;
+  else if (flower1%2!=0 && flower2%2==0) return true;
+  else return false;
 }
 
 console.log(lovefunc(1, 4));
