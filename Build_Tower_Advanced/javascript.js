f8751ca84e2254facaf21ff3973f05c9a25eaefe
Stanislau Kakhnovich <stas_2002@tut.by>
2022-07-14 12:40:13 +0300

    the task

---
+++ Build_Tower_Advanced/javascript.js
@@ -0,0 +1,43 @@
+// Build Tower Advanced
+// Build Tower by the following given arguments:
+// number of floors (integer and always greater than 0)
+// block size (width, height) (integer pair and always greater than (0, 0))
+
+// Tower block unit is represented as *
+
+// Python: return a list;
+// JavaScript: returns an Array;
+// Have fun!
+
+// for example, a tower of 3 floors with block size = (2, 3) looks like below
+
+// [
+//   '    **    ',
+//   '    **    ',
+//   '    **    ',
+//   '  ******  ',
+//   '  ******  ',
+//   '  ******  ',
+//   '**********',
+//   '**********',
+//   '**********'
+// ]
+// and a tower of 6 floors with block size = (2, 1) looks like below
+
+// [
+//   '          **          ', 
+//   '        ******        ', 
+//   '      **********      ', 
+//   '    **************    ', 
+//   '  ******************  ', 
+//   '**********************'
+// ]
+
+
+function towerBuilder(nFloors, nBlockSz) {
+  
+}
+
+
+console.log(towerBuilder(1, [1, 1]));
+console.log(towerBuilder(3, [4, 2]));
