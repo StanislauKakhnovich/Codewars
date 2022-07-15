0edef5b1ea03eba03024f6e6d57bc40338c50f55
Stanislau Kakhnovich <stas_2002@tut.by>
2022-07-15 12:44:58 +0300

    reg exp

--- Detect_Panagram/javascript.js
+++ Detect_Panagram/javascript.js
@@ -5,9 +5,9 @@
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
 function isPangram(string){
-
+  return /a|b/i.test(string);
 }
 
 
 console.log(isPangram("The quick brown fox jumps over the lazy dog"));
-
+console.log(isPangram("Theb"));
