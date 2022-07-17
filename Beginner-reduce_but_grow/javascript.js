6909ffc78f36dec1403a975c5d314b14fa97ebc7
Stanislau Kakhnovich <stas_2002@tut.by>
2022-07-17 11:04:17 +0300

    correct solution

---
+++ Beginner-reduce_but_grow/javascript.js
@@ -0,0 +1,11 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+  return x.reduce((mult, elem)=>mult*elem, 1);
+}
+
+
+console.log(grow([1, 2, 3]));
+console.log(grow([4, 1, 1, 1, 4]));
